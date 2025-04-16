712195cc48e7a4ade26fd3ece53bc0dd57e9272d
ayden-g <ayden.garrett@wsu.edu>
2025-04-15 17:46:44 -0700

    added clarity

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     orders, prior, train_labels, products = load_data()
     features, user_product = create_profile(orders, prior)
     X, y = build_model(orders, train_labels, user_product, features)
-    model, X_full, f1 = train_model(X, y)
+    model, X_set, f1 = train_model(X, y)
 
-    user_id_demo = 1
+    user_id = 1
     recs = recommender(
-        user_id_demo,
+        user_id,
         user_product,
         features[["user_id", "user_total_orders_clean"]],
         features[["product_id", "product_total_purchases_clean", "product_avg_cart_order_clean"]],
